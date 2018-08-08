d262204aa62256480d0f3ce3d1924792af13e2f1
seva <seva.voloshin@gmail.com>
2018-08-09 00:13:54 +0300

    basic functional added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import wordsData from './words.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      words: wordsData,
+      currentName: 'Benedict Cumberbatch'
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    var givenPart1 = this.state.words.givenPart1;
+    var givenPart2 = this.state.words.givenPart2;
+    var surnamePart1 = this.state.words.surnamePart1;
+    var surnamePart2 = this.state.words.surnamePart2;
+    var name = givenPart1[Math.floor(Math.random() * givenPart1.length)] +
+               givenPart2[Math.floor(Math.random() * givenPart2.length)];
+    var surname = surnamePart1[Math.floor(Math.random() * surnamePart1.length)] +
+                  surnamePart2[Math.floor(Math.random() * surnamePart2.length)];
+    this.setState({currentName: `${name} ${surname}`});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p>{ this.state.currentName }</p>
+        <button onClick={ this.handleClick }>
+          What is his name again?
+        </button>
       </div>
     );
   }
